b36f44d5a8dcc19cbdc5f0fe0cd29843c9fc6669
Brandon <brandonmarion@billericak12.com>
2019-10-22 21:25:09 -0400

    intellii

--- src/com/company/Line.java
+++ src/com/company/Line.java
@@ -15,21 +15,27 @@ public class Line {
         end1 = new Point();
         end2 = new Point();
     }
-    /*
     public double distance()
     {
-        //You may find Math.pow(base, power) and Math.sqrt(num) useful
+        double a = Math.pow(end2.getX()-end1.getX(),2);
+        double b = Math.pow(end2.getY()-end1.getY(),2);
+        double c = Math.sqrt(a + b);
+        return c;
     }
     public Point midpoint()
     {
-
+       double a = (end1.getX() + end2.getX())/2;
+       double b = (end1.getY() + end2.getY())/2;
+       Point p =  new Point(a,b);
+        return p;
     }
 
     public boolean sameLength(Line otherLine)
     {
-
+       double l1 = distance();
+       double l2 = otherLine.distance();
+       return (l1==l2);
     }
-    */
     public double slope()
     {
         double m;
@@ -67,7 +73,10 @@ public class Line {
         System.out.println(l2);
         System.out.println("Slope = " + l2.slope());
         System.out.println("Parallel? " + l1.parallel(l2));
-
+        System.out.println("distance = " + l1.distance());
+        System.out.println("Midpoint = " + l1.midpoint());
+        System.out.println("distance = " + l2.distance());
+        System.out.println("Same Length?" + l1.sameLength(l2));
 
         //double dist = l1.distance();
         //Point p1 = l1.midpoint();
@@ -81,3 +90,14 @@ public class Line {
     }
 
 }
+//Line with endpoints (-1.0, -3.0) and (2.0, 1.0)
+//Slope = 1.3333333333333333
+//Line with endpoints (0.0, 0.0) and (3.0, 4.0)
+//Slope = 1.3333333333333333
+//Parallel? true
+//distance = 5.0
+//Midpoint = (0.5, -1.0)
+//distance = 5.0
+//Same Length?true
+//
+//Process finished with exit code 0
